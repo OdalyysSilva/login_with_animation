9c0037b02f45d3cb450560148ed0254e81cee1ef
Odalys Silva <margely2604@hotmail.com>
2025-09-04 10:52:25 -0600

    feat(login): add email and passaword inputs

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -9,18 +9,70 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool _isPasswordVisible =
+      false; //Para controlar la visiblilidad de la contraseña
+
   @override
   Widget build(BuildContext context) {
+    // para obetener el tamaño de la pantalla del dispositivo
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
+      // evita nugde o cámaras frontales para móviles
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-              child: RiveAnimation.asset('assets/animated_loginCharacter.riv'),
-            ),
-          ],
+        child: Padding(
+          // eje x horizontal o derecha izquierda
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              SizedBox(
+                width: size.width,
+                height: 200,
+                child: RiveAnimation.asset(
+                  'assets/animated_login_character.riv',
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                //Para que aparezaca el email del usuraio en móviles
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  hintText: "Email",
+                  prefixIcon: const Icon(Icons.mail),
+                  border: OutlineInputBorder(
+                    // Esquinas redondeadas
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              TextField(
+                //Para que aparezaca la contraseña del usuraio en móviles
+                obscureText: !_isPasswordVisible, // Si es falso, oculta,
+                decoration: InputDecoration(
+                  hintText: "Password",
+                  prefixIcon: const Icon(Icons.lock),
+                  border: OutlineInputBorder(
+                    // Esquinas redondeadas
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility_off
+                          : Icons.visibility,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
-      ), //evita nudge o cámaras frontales para móviles
+      ),
     );
   }
 }
