039b7613db72d829eb50fe14b171fb79fbc5a6c1
Odalys Silva <margely2604@hotmail.com>
2025-09-18 11:06:03 -0600

    feat(rive): wire up ischecking/isHandsUp inputs on login

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,28 @@ import 'package:flutter/material.dart';
 import 'package:login_with_animation/screens/login_screen.dart';
 
 void main() {
+  //Para que la aplicación arranque y aparezca en pantalla
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  //Es la raiz de la app
   const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      debugShowCheckedModeBanner: false,
+      //Configura el tema y la pantalla inicial (temas, navegación, idioma, etc.).
+      title: 'Flutter Demo', //Nombre de la app
+      debugShowCheckedModeBanner: false, //Quita la marca roja de la esquina
       theme: ThemeData(
+        //Define colores y estilos
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const LoginScreen(),
+      home:
+          const LoginScreen(), //Es la pantalla inicial de la app, si se quita solo se verá una pantalla blanca
     );
   }
 }
--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -6,33 +6,72 @@ class LoginScreen extends StatefulWidget {
 
   @override
   State<LoginScreen> createState() => _LoginScreenState();
-}
+} //Si se quita la pantalla no existe
 
 class _LoginScreenState extends State<LoginScreen> {
   bool _isPasswordVisible =
-      false; //Para controlar la visiblilidad de la contraseña
+      false; //Para controlar la visiblilidad de la contraseña, si se quita no podrá mostrar u ocultar
+
+  StateMachineController? controller;
+
+  // Cerebro de la lógica de las animaciones
+  //SMI: State Machine Input
+  SMIBool? isChecking; //Activa el modo chismoso
+  SMIBool? isHandsUp; //se tapa los ojos
+  SMITrigger? trigSucess; //se emociona
+  SMITrigger? trigFail; //Se pone triste
 
   @override
   Widget build(BuildContext context) {
-    // para obetener el tamaño de la pantalla del dispositivo
+    //Si el build se quita, no muestra nada
+    // el MediaQuery para obetener el tamaño de la pantalla del dispositivo
     final Size size = MediaQuery.of(context).size;
     return Scaffold(
+      //Estructura para pantallas
       // evita nugde o cámaras frontales para móviles
       body: SafeArea(
+        //Evita que el contenido se superponga
         child: Padding(
-          // eje x horizontal o derecha izquierda
+          // Margen horizontal, eje x horizontal o derecha izquierda, sino todo se pegaría a los bordes
           padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
+            //Organización vertical, para poner cosas debajo de otras
             children: [
               SizedBox(
+                //Da el espacio para mostrar la animación
                 width: size.width,
                 height: 200,
                 child: RiveAnimation.asset(
+                  //Muestra el archivo de animación
                   'assets/animated_login_character.riv',
+                  stateMachines: ["Login Machine"], //Al iniciarse
+                  onInit: (artboard) {
+                    controller = StateMachineController.fromArtboard(
+                      artboard,
+                      "Login Machine",
+                    );
+                    //Verificar que inició bien
+                    if (controller == null) return;
+                    artboard.addController(controller!);
+                    isChecking = controller!.findSMI('isChecking');
+                    isHandsUp = controller!.findSMI('isHandsUp');
+                    trigSucess = controller!.findSMI('trigSucess');
+                    trigFail = controller!.findSMI('trigFail');
+                  },
                 ),
               ),
               const SizedBox(height: 10),
               TextField(
+                onChanged: (value) {
+                  if (isHandsUp != null) {
+                    //No tapar los ojos al escribir email
+                    isHandsUp!.change(false);
+                  }
+                  if (isChecking == null) return;
+                  //Activa el modo chismoso
+                  isChecking!.change(true);
+                },
+                //Campo de texto
                 //Para que aparezaca el email del usuraio en móviles
                 keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
@@ -46,13 +85,23 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               const SizedBox(height: 10),
               TextField(
+                onChanged: (value) {
+                  if (isChecking != null) {
+                    //No tapar los ojos al escribir email
+                    isChecking!.change(false);
+                  }
+                  if (isHandsUp == null) return;
+                  //Activa el modo chismoso
+                  isHandsUp!.change(true);
+                },
                 //Para que aparezaca la contraseña del usuraio en móviles
-                obscureText: !_isPasswordVisible, // Si es falso, oculta,
+                obscureText:
+                    !_isPasswordVisible, // Si es true, oculta y si es false, muestra
                 decoration: InputDecoration(
                   hintText: "Password",
                   prefixIcon: const Icon(Icons.lock),
                   border: OutlineInputBorder(
-                    // Esquinas redondeadas
+                    // BorderRadius es para esquinas redondeadas
                     borderRadius: BorderRadius.circular(12),
                   ),
                   suffixIcon: IconButton(
@@ -67,6 +116,47 @@ class _LoginScreenState extends State<LoginScreen> {
                       });
                     },
                   ),
+                ),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: size.width,
+                child: const Text(
+                  "Forgot your password",
+                  textAlign: TextAlign.right, //Alinear a la derecha
+                  style: TextStyle(decoration: TextDecoration.underline),
+                ),
+              ),
+              const SizedBox(height: 10), //Para el botón de entrada
+              MaterialButton(
+                minWidth: size.width,
+                height: 50,
+                color: Colors.deepPurpleAccent,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadiusGeometry.circular(12),
+                ),
+                onPressed: () {},
+                child: Text("Login", style: TextStyle(color: Colors.white70)),
+              ),
+              const SizedBox(height: 10),
+              SizedBox(
+                width: size.width,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text("Don't have an account?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Register",
+                        style: TextStyle(
+                          color: Colors.black12,
+                          fontWeight: FontWeight.bold,
+                          decoration: TextDecoration.underline,
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
               ),
             ],
